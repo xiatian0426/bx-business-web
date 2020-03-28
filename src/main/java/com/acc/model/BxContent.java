953bbb798e3e60e00526aec105b89ffd21d3919c
xugs <xugshang@163.com>
2020-03-28 15:46:26 +0800

    提交

--- src/main/java/com/acc/model/BxContent.java
+++ src/main/java/com/acc/model/BxContent.java
@@ -23,7 +23,15 @@ public class BxContent implements java.io.Serializable, Cloneable{
 
     private String contentImg;
 
-    private Integer contentImgType;//0:图片 1:文字
+    private String contentDesc;
+
+    private String reserve1;
+
+    private String reserve2;
+
+    private String reserve3;
+
+    private Integer contentImgType;//0:图片 1:文字 2:视频
 	
 	private Integer contentOrder;
 
@@ -140,6 +148,38 @@ public class BxContent implements java.io.Serializable, Cloneable{
 
     public void setContentImgType(Integer contentImgType) {
         this.contentImgType = contentImgType;
+    }
+
+    public String getContentDesc() {
+        return contentDesc;
+    }
+
+    public void setContentDesc(String contentDesc) {
+        this.contentDesc = contentDesc;
+    }
+
+    public String getReserve1() {
+        return reserve1;
+    }
+
+    public void setReserve1(String reserve1) {
+        this.reserve1 = reserve1;
+    }
+
+    public String getReserve2() {
+        return reserve2;
+    }
+
+    public void setReserve2(String reserve2) {
+        this.reserve2 = reserve2;
+    }
+
+    public String getReserve3() {
+        return reserve3;
+    }
+
+    public void setReserve3(String reserve3) {
+        this.reserve3 = reserve3;
     }
 }
 
